fd81ef73f29a9400226ce232893486f603e2e8b8
Thammineni Naveen Kumar <146698544+Naveenravana@users.noreply.github.com>
2026-01-03 22:49:25 +0530

    Add files via upload

--- Script.js
+++ Script.js
@@ -5,24 +5,27 @@ gsap.from(".hero-content > *", {
   opacity: 0,
   y: 30,
   duration: 1,
-  stagger: 0.2
+  stagger: 0.2,
+  ease: "power3.out"
 });
 
 // Cards animation
-gsap.from(".value-card, .work-card, .project, .timeline-item", {
+gsap.from(".value-card, .work-card", {
   scrollTrigger: {
-    trigger: "section",
-    start: "top 85%"
+    trigger: ".value",
+    start: "top 80%"
   },
   opacity: 0,
   y: 40,
   duration: 0.8,
-  stagger: 0.1
+  stagger: 0.2
 });
 
 // Scroll progress
 window.addEventListener("scroll", () => {
-  const progress = (window.scrollY /
-    (document.body.scrollHeight - window.innerHeight)) * 100;
+  const progress =
+    (window.scrollY /
+      (document.body.scrollHeight - window.innerHeight)) *
+    100;
   document.getElementById("scroll-progress").style.width = progress + "%";
 });
